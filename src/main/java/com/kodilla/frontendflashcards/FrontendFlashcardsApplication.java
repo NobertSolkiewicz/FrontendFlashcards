861983a5039a4735e07103e87d9dd9c9654f6686
Norbert <norsol2200@gmail.com>
2023-09-22 14:53:03 +0200

    First commit

---
+++ src/main/java/com/kodilla/frontendflashcards/FrontendFlashcardsApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.frontendflashcards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendFlashcardsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrontendFlashcardsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kodilla/frontendflashcards/FrontendFlashcardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.frontendflashcards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontendFlashcardsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
